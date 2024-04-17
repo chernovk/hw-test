8e0aba09af560da73c97ccc96663dbd54cc37f0f
Konstantin <konstantin@Konstantins-MacBook-Air.local>
2024-04-17 18:45:47 +0400

    init

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,33 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	dict := make(map[string]int)
+
+	for _, word := range strings.Fields(text) {
+		dict[word] = dict[word] + 1
+
+	}
+
+	words := make([]string, 0, len(dict))
+	for word := range dict {
+		words = append(words, word)
+
+	}
+
+	sort.Slice(words, func(i, j int) bool {
+		if dict[words[i]] == dict[words[j]] {
+			return words[i] < words[j]
+		}
+		return dict[words[i]] > dict[words[j]]
+	})
+
+	if len(words) > 10 {
+		words = words[:10]
+	}
+	return words
 }
