425821fd0132d44f54d16adcd7ac7028f50c54f0
Konstantin <konstantin@Konstantins-MacBook-Air.local>
2024-06-25 00:06:00 +0400

    08 done

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	arguments := os.Args
+	envDirectory := arguments[1]
+	environments, err := ReadDir(envDirectory)
+	if err != nil {
+		log.Println(err.Error())
+		os.Exit(1)
+	}
+
+	exitCode := RunCmd(arguments[2:], environments)
+	os.Exit(exitCode)
 }
