b7aa76fa6a1c5b565402fc53dc1076c378211515
Konstantin <konstantin@Konstantins-MacBook-Air.local>
2024-09-09 22:33:54 +0400

    done

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -1,3 +1,4 @@
+//go:build !bench
 // +build !bench
 
 package hw10programoptimization
@@ -14,6 +15,7 @@ func TestGetDomainStat(t *testing.T) {
 {"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
 {"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
 {"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":4,"Name":"Ivan Ivanov","Username":"tIvan","Email":"Ivan@hello.co.uk","Phone":"520-04-11","Password":"paSSword","Address":"Lenina 20"}
 {"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
 
 	t.Run("find 'com'", func(t *testing.T) {
@@ -35,5 +37,11 @@ func TestGetDomainStat(t *testing.T) {
 		result, err := GetDomainStat(bytes.NewBufferString(data), "unknown")
 		require.NoError(t, err)
 		require.Equal(t, DomainStat{}, result)
+	})
+
+	t.Run("find 'co.uk'", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(data), "co.uk")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{"hello.co.uk": 1}, result)
 	})
 }
